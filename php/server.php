1951e20c458c8e7748c1944457c12aa4d8224c42
Dave Albert <dave.albert@publicisdhealthcare.ie>
2014-03-03 09:57:34 +0000

    PHP Server can have text pushed to it now.

--- php/server.php
+++ php/server.php
@@ -1,3 +1,14 @@
 <?php
- echo 'up';
-?>
+
+$file = 'log.txt';
+if (isset($_GET['text']))
+{
+   $current = $_GET['text'];
+   file_put_contents($file, $current);
+
+} else {
+   $current = file_get_contents($file);
+}
+
+
+echo $current;
